c6d6da0e595d1a031fc1de027128d38abdc1ac41
hasitha <hasithadkr7@gmail.com>
2020-09-14 13:13:55 +0530

    test in win2

--- input/input_prep.py
+++ input/input_prep.py
@@ -3,22 +3,34 @@ import sys
 import os
 from google.cloud import storage
 
+MATLAB_DIR = r"E:\MIKE\ProductionRun\hourly_run\Matlab"
+KEY_FILE = r"E:\MIKE\ProductionRun\hourly_run\uwcc-admin\uwcc-admin.json"
+
 
 def download_rain_input_files(bucket_time, config):
     try:
-        download_input_files(bucket_time, config['gcp_service_key'], config['output_dir'], config['bucket_name'],
-                             config['mike_rf_bucket_file'], config['input_rain_file'])
+        download_input_files(bucket_time, KEY_FILE, MATLAB_DIR, config['bucket_name'],
+                             config['input_rain_file'], config['input_rain_file'])
     except Exception as e:
-        print('download_files|Exception : ', str(e))
+        print('download_rain_input_files|Exception : ', str(e))
         return False
 
 
 def download_tide_input_files(bucket_time, config):
     try:
-        download_input_files(bucket_time, config['gcp_service_key'], config['output_dir'], config['bucket_name'],
-                             config['mike_tide_bucket_file'], config['input_tide_file'])
+        download_input_files(bucket_time, KEY_FILE, MATLAB_DIR, config['bucket_name'],
+                             config['input_tide_file'], config['input_tide_file'])
     except Exception as e:
-        print('download_files|Exception : ', str(e))
+        print('download_tide_input_files|Exception : ', str(e))
+        return False
+
+
+def download_discharge_input_files(bucket_time, config):
+    try:
+        download_input_files(bucket_time, KEY_FILE, MATLAB_DIR, config['bucket_name'],
+                             config['input_discharge_file'], config['input_discharge_file'])
+    except Exception as e:
+        print('download_discharge_input_files|Exception : ', str(e))
         return False
 
 
@@ -26,7 +38,7 @@ def download_input_files(bucket_time, key_file, output_dir, bucket_name, src_fil
     try:
         source_blob_name = 'mike/inputs/{}/{}'.format(bucket_time, src_file)
         print('download_rain_input_files|source_blob_name : ', source_blob_name)
-        destination_file_name = '{}/{}/{}'.format(output_dir, bucket_time, dest_file)
+        destination_file_name = '{}/{}'.format(output_dir, dest_file)
         print('download_rain_input_files|destination_file_name : ', destination_file_name)
         storage_client = storage.Client.from_service_account_json(key_file)
         print('download_rain_input_files|storage_client : ', storage_client)
@@ -40,3 +52,21 @@ def download_input_files(bucket_time, key_file, output_dir, bucket_name, src_fil
     except Exception as e:
         print('download_files|Exception : ', str(e))
         return False
+
+
+def run_matlab_input_preparation():
+    print('')
+
+
+def upload_file_to_bucket(key_file, bucket_name, source_file_name, destination_blob_name):
+    try:
+        client = storage.Client.from_service_account_json(key_file)
+        bucket = client.get_bucket(bucket_name)
+        blob = bucket.blob(destination_blob_name)
+        blob.upload_from_filename(source_file_name)
+        print("File {} uploaded to {}.".format(source_file_name, destination_blob_name))
+        return True
+    except Exception as e:
+        print('upload_file_to_bucket|Exception : ', str(e))
+        return False
+
--- windows_scripts/matlab_run.bat
+++ windows_scripts/matlab_run.bat
@@ -1,1 +1,1 @@
-matlab -nodisplay -nosplash -nodesktop -r "run('read_res11_WL.m');exit;"
+matlab -nodisplay -nosplash -nodesktop -r "run('create_dfs0_MIKE_Inputs.m');exit;"
