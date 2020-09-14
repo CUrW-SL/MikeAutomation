5eb5a8ec3afbb26aaa48ebdd16dd5a0c83b38325
hasitha <hasithadkr7@gmail.com>
2020-09-14 15:07:19 +0530

    matlab bat cmd update

--- windows_scripts/matlab_run.bat
+++ windows_scripts/matlab_run.bat
@@ -1,1 +1,1 @@
-matlab -nodisplay -nosplash -nodesktop -r "run('E:\MIKE\ProductionRun\hourly_run\Matlab\create_dfs0_MIKE_Inputs.m');exit;"
+matlab -nodisplay -nosplash -nodesktop -r "run('E:\MIKE\ProductionRun\hourly_run\Matlab\input_processor.m');exit;"
