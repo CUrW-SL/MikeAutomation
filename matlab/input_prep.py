6167b8c212aaf11b1b5355dc907f3059aa6788eb
hasitha <hasithadkr7@gmail.com>
2020-09-23 12:08:07 +0530

    add sleep to mike input prep upload

--- matlab/input_prep.py
+++ matlab/input_prep.py
@@ -58,7 +58,7 @@ def run_matlab_input_preparation():
         command = '.\windows_scripts\matlab_input_process.bat'
         print('run_matlab_input_preparation|command: ', command)
         subprocess.call(command, shell=True)
-        time.sleep(5)
+        time.sleep(10)
         print('run_matlab_input_preparation|completed')
         return True
     except Exception as ex:
